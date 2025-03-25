b4a168de476c48270e34d99589fc66a6346399b7
MiltonRodriguez37 <milton.rodriguezg37@gmail.com>
2025-03-25 16:27:41 -0600

    configuracion server para despliegue

--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db')
 const cors = require('cors');
 const app = express();
 
+const port = process.env.PORT || 8080;
+
 connectDB();
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../frontend/dist/frontend')));
 
 app.use('/api/products', require('./routes/product'))
 app.use('/api/users', require('./routes/user'))
 
-app.listen(4000, ()=>{
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/frontend/index.html'));
+});
+
+app.listen(port, ()=>{
   console.log('El servidor está corriendo')
 })
