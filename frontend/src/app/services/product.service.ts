1507c042e4aa14bef3bf9e3079f336eed65784eb
MiltonRodriguez37 <milton.rodriguezg37@gmail.com>
2025-03-25 21:19:57 -0600

    Ajuste url front

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class ProductService {
   /* url = 'http://localhost:8080/api/products/'; */
-  url = 'https://crud-angular-express.onrender.com/api/products';
+  url = 'https://crud-angular-express.onrender.com/api/products/';
 /*     listProducts: Product[] = [
       {product: 'Laptop HP Pavilion', description: 'Laptop con pantalla de 15.6", 8GB RAM, SSD 256GB, Intel Core i5.', category: 'Electrónicos', price: 699.99, stock: 15},
       {product: 'Balón de fútbol Adidas', description: 'Balón oficial tamaño 5, para partidos profesionales.', category: 'Deportes', price: 49.99, stock: 10},
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
   /* url = 'http://localhost:8080/api/users/'; */
-  url = 'https://crud-angular-express.onrender.com/api/users';
+  url = 'https://crud-angular-express.onrender.com/api/users/';
 /*     listUsers: User[] = [
       {name: 'Milton Osvaldo', lastname: 'Rodríguez González', gender: 'Femenino', telephone: '3315897034'},
       {name: 'Milton Osvaldo', lastname: 'Rodríguez González', gender: 'Femenino', telephone: '3315897034'},
